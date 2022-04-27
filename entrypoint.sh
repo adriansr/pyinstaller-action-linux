e5a7b113b640828593f4e4ba353e3faf5aef5db0
Adrian Serrano <adrisr83@gmail.com>
2022-04-27 16:50:58 +0200

    Use python3 from base image

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ WORKDIR=${SRCDIR:-/src}
 
 SPEC_FILE=${4:-*.spec}
 
-/root/.pyenv/shims/python -m pip install --upgrade pip wheel setuptools
+python3 -m pip install --upgrade pip wheel setuptools
 
 #
 # In case the user specified a custom URL for PYPI, then use
@@ -42,9 +42,9 @@ fi
 cd $WORKDIR
 
 if [ -f requirements.txt ]; then
-    /root/.pyenv/shims/pip install -r requirements.txt
+    pip3 install -r requirements.txt
 fi # [ -f requirements.txt ]
 
-/root/.pyenv/shims/pyinstaller --clean -y --dist ./dist/linux --workpath /tmp $SPEC_FILE
+pyinstaller --clean -y --dist ./dist/linux --workpath /tmp $SPEC_FILE
 
 chown -R --reference=. ./dist/linux
